6999c41d825a141668a46013e8e1b7d299b1b4f3
Leonardo Ferreira Luciano <lferreiraluciano@gmail.com>
2021-03-01 21:12:59 -0300

    Run create subsidiaries migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_205957) do
+ActiveRecord::Schema.define(version: 2021_03_01_214525) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name", limit: 191
@@ -20,4 +20,15 @@ ActiveRecord::Schema.define(version: 2021_03_01_205957) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "subsidiaries", force: :cascade do |t|
+    t.string "name", limit: 191
+    t.string "legal_name", limit: 191
+    t.string "document", limit: 13
+    t.integer "company_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["company_id"], name: "index_subsidiaries_on_company_id"
+  end
+
+  add_foreign_key "subsidiaries", "companies"
 end
