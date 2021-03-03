838a8691125482bb7f0d5df8f365eeca60f08310
Leonardo Ferreira Luciano <lferreiraluciano@gmail.com>
2021-03-03 20:13:42 -0300

    Run migration for create join table subsidiaries_customers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_03_225720) do
+ActiveRecord::Schema.define(version: 2021_03_03_231042) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name", limit: 191
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 2021_03_03_225720) do
     t.string "phone", limit: 14
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "customers_subsidiaries", id: false, force: :cascade do |t|
+    t.integer "subsidiary_id", null: false
+    t.integer "customer_id", null: false
+    t.index ["subsidiary_id", "customer_id"], name: "index_customers_subsidiaries_on_subsidiary_id_and_customer_id"
   end
 
   create_table "subsidiaries", force: :cascade do |t|
