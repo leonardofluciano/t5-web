2e6728e21149c914d0a822347df78a654268e798
Leonardo Ferreira Luciano <lferreiraluciano@gmail.com>
2021-03-15 20:35:30 -0300

    Add validations for subsidiaries CRUD

--- app/models/subsidiary.rb
+++ app/models/subsidiary.rb
@@ -1,4 +1,8 @@
 class Subsidiary < ApplicationRecord
     belongs_to :company
     has_and_belongs_to_many :customers
+
+    validates :name, length: { maximum: 191 }
+    validates :legal_name, length: { maximum: 191 }
+    validates :document, length: { is: 14 }, document: true, uniqueness: true
 end
