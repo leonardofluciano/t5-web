c8f348ddd9b3c8e91f126b76e6e43f81f8df0b4f
Leonardo Ferreira Luciano <lferreiraluciano@gmail.com>
2021-03-15 20:33:38 -0300

    Add uniqueness validation for document

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,5 +3,5 @@ class Company < ApplicationRecord
 
     validates :name, length: { maximum: 191 }
     validates :legal_name, length: { maximum: 191 }
-    validates :document, length: { is: 14 }, document: true
+    validates :document, length: { is: 14 }, document: true, uniqueness: true
 end
