8fee5fc315e0f26706da716c2d43797427eaae9c
Leonardo Ferreira Luciano <lferreiraluciano@gmail.com>
2021-03-15 20:28:08 -0300

    Fix cnpj validation

--- app/models/concerns/document_validator.rb
+++ app/models/concerns/document_validator.rb
@@ -8,14 +8,33 @@ class DocumentValidator < ActiveModel::EachValidator
     private
 
     def validateCnpj cnpj
+        if (
+            cnpj == '00000000000000' ||
+            cnpj == '11111111111111' ||
+            cnpj == '22222222222222' ||
+            cnpj == '33333333333333' ||
+            cnpj == '44444444444444' ||
+            cnpj == '55555555555555' ||
+            cnpj == '66666666666666' ||
+            cnpj == '77777777777777' ||
+            cnpj == '88888888888888' ||
+            cnpj == '99999999999999'
+        )
+            return false
+        end
+
         cnpjArray = cnpj.split ''
 
         digitSum1 = 0
         cnpjArray.slice(0, 12).reverse().each_with_index do |item, index|
+            puts (item.to_i)
+            puts (((index.to_i % 8) + 2))
+            puts ('######')
             digitSum1 += item.to_i * ((index.to_i % 8) + 2)
         end
 
-        if (11 - (digitSum1 % 11) != cnpjArray[12].to_i)
+        mod1 = digitSum1 % 11
+        if (mod1 != 0 && mod1 != 1 && 11 - (digitSum1 % 11) != cnpjArray[12].to_i)
             return false
         end
 
@@ -24,7 +43,8 @@ class DocumentValidator < ActiveModel::EachValidator
             digitSum2 += item.to_i * ((index.to_i % 8) + 2)
         end
 
-        if (11 - (digitSum2 % 11) != cnpjArray[13].to_i)
+        mod2 = digitSum2 % 11
+        if (mod2 != 0 && mod2 != 1 && 11 - (digitSum2 % 11) != cnpjArray[13].to_i)
             return false
         end
 
