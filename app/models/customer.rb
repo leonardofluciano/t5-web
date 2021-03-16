70d035f820839a719a0118f9852db8e35a36fc96
Leonardo Ferreira Luciano <lferreiraluciano@gmail.com>
2021-03-16 17:32:43 -0300

    Add any validation for customer document

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,8 @@
 class Customer < ApplicationRecord
     has_and_belongs_to_many :subsidiaries
     has_one :account, dependent: :destroy
+
+    validates :name, length: { maximum: 191 }
+    validates :surname, length: { maximum: 191 }
+    validates :document, length: { minimum: 11, maximum: 14 }, document: { type: 'any' }, uniqueness: true
 end
