e4b66ff911983c1544d8d99defb55ac8c69477f7
Leonardo Ferreira Luciano <lferreiraluciano@gmail.com>
2021-03-16 17:30:00 -0300

    Specify cnpj validation for company

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,5 +3,5 @@ class Company < ApplicationRecord
 
     validates :name, length: { maximum: 191 }
     validates :legal_name, length: { maximum: 191 }
-    validates :document, length: { is: 14 }, document: true, uniqueness: true
+    validates :document, length: { is: 14 }, document: { type: 'cnpj' }, uniqueness: true
 end
