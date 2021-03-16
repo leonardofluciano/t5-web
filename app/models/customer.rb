32fe74311c1045b31d6810b4a134f8a8cc980df2
Leonardo Ferreira Luciano <lferreiraluciano@gmail.com>
2021-03-16 17:47:39 -0300

    Add presence validation for customer name and surname

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,7 +2,7 @@ class Customer < ApplicationRecord
     has_and_belongs_to_many :subsidiaries
     has_one :account, dependent: :destroy
 
-    validates :name, length: { maximum: 191 }
-    validates :surname, length: { maximum: 191 }
+    validates :name, length: { maximum: 191 }, presence: true
+    validates :surname, length: { maximum: 191 }, presence: true
     validates :document, length: { minimum: 11, maximum: 14 }, document: { type: 'any' }, uniqueness: true
 end
