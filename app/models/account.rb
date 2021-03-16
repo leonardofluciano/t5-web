5df98065ca62f7e0ea2819679b50c4aad9b3f785
Leonardo Ferreira Luciano <lferreiraluciano@gmail.com>
2021-03-16 17:47:58 -0300

    Add validations for account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,6 @@
 class Account < ApplicationRecord
     belongs_to :customer
+
+    validates :number, length: { is: 12 }, numericality: { only_integer: true, greater_than: 0 }
+    validates :balance, length: { maximum: 191 }, numericality: { greater_than_or_equal: 0 }
 end
