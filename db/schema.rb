b387eaba1ea0db8f5ae7328f3a3560081953f640
Leonardo Ferreira Luciano <lferreiraluciano@gmail.com>
2021-03-20 18:07:19 -0300

    Run all migrations in development

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,54 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_03_06_180503) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.string "number"
+    t.float "balance"
+    t.bigint "customer_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_accounts_on_customer_id"
+  end
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "legal_name"
+    t.string "document"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.string "surname"
+    t.string "document"
+    t.string "email"
+    t.string "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "customers_subsidiaries", id: false, force: :cascade do |t|
+    t.bigint "subsidiary_id", null: false
+    t.bigint "customer_id", null: false
+    t.index ["subsidiary_id", "customer_id"], name: "index_customers_subsidiaries_on_subsidiary_id_and_customer_id"
+  end
+
+  create_table "subsidiaries", force: :cascade do |t|
+    t.string "name"
+    t.string "legal_name"
+    t.string "document"
+    t.bigint "company_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["company_id"], name: "index_subsidiaries_on_company_id"
+  end
+
+  add_foreign_key "accounts", "customers"
+  add_foreign_key "subsidiaries", "companies"
 end
