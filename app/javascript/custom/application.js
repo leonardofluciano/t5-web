caa4a142c9902af8e1aa9c347f14213ff6af375a
Leonardo Ferreira Luciano <lferreiraluciano@gmail.com>
2021-03-22 19:17:32 -0300

    Add dynamic colors to navbar links

--- app/javascript/custom/application.js
+++ app/javascript/custom/application.js
@@ -6,4 +6,17 @@ Inputmask.extendDefaults({
 
 document.addEventListener("turbolinks:load", function() {
     Inputmask().mask(document.querySelectorAll('input'))
+        
+    let allowedCrudNames = [ 'companies', 'subsidiaries', 'customers', 'accounts' ] 
+    let crudNamesRegex   = new RegExp(`^\/(${allowedCrudNames.join('|')})($|\/.*)`)
+    let crudName         = window.location.pathname.replace(crudNamesRegex, '$1')
+    
+    if (allowedCrudNames.includes(crudName)) {
+        $('.web-nav-link').removeClass('text-primary')
+        $('.web-nav-link').addClass('text-secondary')
+
+        let elemString = `#web-nav-link-${crudName}`
+        $(elemString).removeClass('text-secondary')
+        $(elemString).addClass('text-primary')
+    }
 })
