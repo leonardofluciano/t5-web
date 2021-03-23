e5bc349254a8ed193e13399a2b1b46db1a8ccb9f
Leonardo Ferreira Luciano <lferreiraluciano@gmail.com>
2021-03-23 16:58:50 -0300

    Alter the color scheme

--- app/javascript/custom/application.js
+++ app/javascript/custom/application.js
@@ -12,11 +12,16 @@ document.addEventListener("turbolinks:load", function() {
     let crudName         = window.location.pathname.replace(crudNamesRegex, '$1')
     
     if (allowedCrudNames.includes(crudName)) {
-        $('.web-nav-link').removeClass('text-primary')
+        $('.web-nav-link').removeClass('bg-primary')
+        $('.web-nav-link').removeClass('text-light')
+        $('.web-nav-link').addClass('bg-light')
         $('.web-nav-link').addClass('text-secondary')
 
         let elemString = `#web-nav-link-${crudName}`
+        $(elemString).removeClass('bg-light')
         $(elemString).removeClass('text-secondary')
-        $(elemString).addClass('text-primary')
+        $(elemString).addClass('web-nav-link-active')
+        $(elemString).addClass('bg-primary')
+        $(elemString).addClass('text-light')
     }
 })
